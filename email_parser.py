7f776d2b3c204830d376f8f6e27998ec2042a878
Shakesbeer333 <julian.metzger.shakesbeer@web.de>
2019-11-13 09:12:27 +0100

    Reduce stored information to ID, FROM, CONTENT, PATH

--- email_parser.py
+++ email_parser.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import re
+import pickle
 
 path = os.path.join(os.getcwd(), "maildir")
 
@@ -11,18 +12,17 @@ for r, d, f in os.walk(path):
         if '.' in file:
             email_list.append(os.path.join(r, file))
 
-# todo Cc und Bcc gibt es nicht immer
 df = pd.DataFrame(
-    columns=["ID", "Date", "From", "To", "Subject", "Cc", "Mime-Version", "Content-Type", "Content-Transfer-Encoding",
-             "Bcc", "Content"])
+    columns=["ID", "Date", "From", "Content", "Path"])
 
-for index, f in enumerate(email_list[:2]):
+for index, f in enumerate(email_list):
 
-    ObjRead = open(f, "r")
+    try:
+        t = open(file=f, encoding="utf-8", mode="r").read()
+    except UnicodeDecodeError:
+        print(f)
+        # t = open(file = f, encoding="utf-16", mode="r").read()
 
-    t = ObjRead.read()
-
-    # print(t)
 
     # Message ID
 
@@ -32,9 +32,6 @@ for index, f in enumerate(email_list[:2]):
     if match_start and match_end:
         df.at[index, "ID"] = t[match_start.end(): match_end.end() - 1]
 
-        # todo delete
-        print(t[match_start.end(): match_end.end() - 1])
-
     # Date
 
     match_start = re.search("Date: ", t)
@@ -42,9 +39,6 @@ for index, f in enumerate(email_list[:2]):
 
     if match_start and match_end:
         df.at[index, "Date"] = t[match_start.end(): match_end.end()]
-
-        # todo delete
-        print(t[match_start.end(): match_end.end()])
 
     # From
 
@@ -54,95 +48,6 @@ for index, f in enumerate(email_list[:2]):
     if match_start and match_end:
         df.at[index, "From"] = t[match_start.end(): match_end.end()]
 
-        # todo delete
-        print(t[match_start.end(): match_end.end()])
-
-    # To
-
-    match_start = re.search("To: ", t)
-    match_end = re.search("Subject: .*", t)
-
-    if match_start and match_end:
-        df.at[index, "To"] = t[match_start.end(): match_end.start() - 1]
-
-        # todo delete
-        print(t[match_start.end(): match_end.start() - 1])
-
-    # Subject
-
-    match_start = re.search("Subject: ", t)
-    match_end = re.search("Cc: .*", t)
-    match_end_alternative = re.search("Mime-Version: .*", t)
-
-    if match_start and match_end:
-        df.at[index, "Subject"] = t[match_start.end(): match_end.start() - 1]
-
-        # todo delete
-        print(t[match_start.end(): match_end.start() - 1])
-
-    elif match_start and match_end_alternative:
-        df.at[index, "Subject"] = t[match_start.end(): match_end_alternative.start() - 1]
-
-        # todo delete
-        print(t[match_start.end(): match_end_alternative.start() - 1])
-
-    # Cc
-
-    match_start = re.search("Cc: ", t)
-    match_end = re.search("Mime-Version: .*", t)
-
-    if match_start and match_end:
-        df.at[index, "Cc"] = t[match_start.end(): match_end.start() - 1]
-
-        # todo delete
-        print(t[match_start.end(): match_end.start() - 1])
-
-    # Mime-Version
-
-    match_start = re.search("Mime-Version: ", t)
-    match_end = re.search("Mime-Version: .*", t)
-
-    if match_start and match_end:
-        df.at[index, "Mime-Version"] = t[match_start.end(): match_end.end()]
-
-        # todo delete
-        print(t[match_start.end(): match_end.end()])
-
-    ObjRead.close()
-
-    # Content-Type
-
-    match_start = re.search("Content-Type: ", t)
-    match_end = re.search("Content-Type: .*", t)
-
-    if match_start and match_end:
-        df.at[index, "Content-Type"] = t[match_start.end(): match_end.end()]
-
-        # todo delete
-        print(t[match_start.end(): match_end.end()])
-
-    # Content-Transfer-Encoding
-
-    match_start = re.search("Content-Transfer-Encoding: ", t)
-    match_end = re.search("Content-Transfer-Encoding: .*", t)
-
-    if match_start and match_end:
-        df.at[index, "Content-Transfer-Encoding"] = t[match_start.end(): match_end.end()]
-
-        # todo delete
-        print(t[match_start.end(): match_end.end()])
-
-    # Bcc
-
-    match_start = re.search("Bcc: ", t)
-    match_end = re.search("X-From: .*", t)
-
-    if match_start and match_end:
-        df.at[index, "Bcc"] = t[match_start.end(): match_end.start() - 1]
-
-        # todo delete
-        print(t[match_start.end(): match_end.start() - 1])
-
     # Content
 
     match_start = re.search("X-FileName: .*", t)
@@ -150,10 +55,10 @@ for index, f in enumerate(email_list[:2]):
     if match_start and match_end:
         df.at[index, "Content"] = t[match_start.end():]
 
-        # todo delete
-        print(t[match_start.end():])
+    # Path
 
-    print("")
-    print("")
+    df.at[index, "Path"] = f.split("maildir")[1]
+
+pickle.dump(df, open(path + "/Data_Pickle/e_mails.p", "wb"))
 
 print(df)
