bce6dacee89ca80f23a6c0666f60a8ae35f453e5
Shakesbeer333 <julian.metzger.shakesbeer@web.de>
2019-11-13 17:17:24 +0100

    - Only consider important employees (184 names provided in txt file) - Only consider emails in year 2001

--- email_parser.py
+++ email_parser.py
@@ -2,13 +2,37 @@ import os
 import pandas as pd
 import re
 import pickle
+from dateutil import parser
 
-path = os.path.join(os.getcwd(), "maildir")
+
+with open(file= os.path.join(os.getcwd(), 'employees.txt'), encoding="utf-8", mode="r") as f:
+    employee_txt = f.readlines()
+
+employee_list = []
+
+for line in employee_txt:
+    whitespace = re.search(r'\s', line)
+    employee = line[0: whitespace.start()]
+    employee_list.append(employee)
+
+f.close()
+
+
+# Email Parsing
+#
+#
+#path = os.path.join(os.getcwd(), "maildir")
+## Testing path
+#
+email_path = os.path.join(os.getcwd(), 'subfolder_email')
 
 email_list = []
 # r=root, d=directories, f = files
-for r, d, f in os.walk(path):
+for r, d, f in os.walk(email_path):
     for file in f:
+        # Don't consider pickle
+        if '.p' in file:
+            continue
         if '.' in file:
             email_list.append(os.path.join(r, file))
 
@@ -23,6 +47,33 @@ for index, f in enumerate(email_list):
         print(f)
         # t = open(file = f, encoding="utf-16", mode="r").read()
 
+    # From
+
+    match_start = re.search("From: ", t)
+    match_end = re.search("From: .*", t)
+
+    if match_start and match_end:
+
+        # Option 1: Consider just within-Enron communication --> @enron
+        # Option 2: Consider entire communication
+
+        user_email = t[match_start.end(): match_end.end()]
+
+        user_name_start = re.search('@', user_email)
+
+        if user_name_start:
+
+            user_name = user_email[0: user_name_start.start()]
+
+            # Only consider important employees
+            if user_name in employee_list:
+                df.at[index, "From"] = user_name
+                pass
+            else:
+                continue
+
+        else:
+            print('No valid E-Mail')
 
     # Message ID
 
@@ -32,21 +83,21 @@ for index, f in enumerate(email_list):
     if match_start and match_end:
         df.at[index, "ID"] = t[match_start.end(): match_end.end() - 1]
 
+
     # Date
 
     match_start = re.search("Date: ", t)
     match_end = re.search("Date: .*", t)
 
     if match_start and match_end:
-        df.at[index, "Date"] = t[match_start.end(): match_end.end()]
+        date = t[match_start.end(): match_end.end()]
+        date_time_obj = parser.parse(date)
 
-    # From
-
-    match_start = re.search("From: ", t)
-    match_end = re.search("From: .*", t)
-
-    if match_start and match_end:
-        df.at[index, "From"] = t[match_start.end(): match_end.end()]
+        if date_time_obj.year == 2001:
+            df.at[index, "Date"] = date
+        else:
+            df.drop([index], inplace = True)
+            continue
 
     # Content
 
@@ -57,8 +108,11 @@ for index, f in enumerate(email_list):
 
     # Path
 
-    df.at[index, "Path"] = f.split("maildir")[1]
+    #df.at[index, "Path"] = f.split('maildir')[1]
+    df.at[index, "Path"] = f.split('subfolder_email')[1]
 
-pickle.dump(df, open(path + "/Data_Pickle/e_mails.p", "wb"))
+
+
+pickle.dump(df, open(email_path + "/Data_Pickle/e_mails.p", "wb"))
 
 print(df)
