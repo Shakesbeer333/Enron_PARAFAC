15ada7159f911bfb2a28ed60b393be19801797fd
Shakesbeer333 <julian.metzger.shakesbeer@web.de>
2019-12-20 13:22:35 +0100

    Composition, Visualization

--- tensor_decomposition.py
+++ tensor_decomposition.py
@@ -1,33 +1,27 @@
 import numpy as np
-from utils import *
 import tensorly as tl
-from tensorly import unfold as tl_unfold
 from tensorly.decomposition import parafac
-import logging
-from sktensor import dtensor, ktensor
-from sktensor import dtensor, cp_als
 import pickle
 from configparser import ConfigParser
 import os
 import matplotlib.pyplot as plt
-import string
+from matplotlib.pyplot import cm
 
 # Load
 parser = ConfigParser()
 parser.read('dev.ini')
-dir_ = parser.get('Parsing', 'dir_', fallback = 'maildir')
+dir_ = parser.get('Parsing', 'dir_', fallback='maildir')
 email_path = os.path.join(os.getcwd(), dir_)
+cwd = os.getcwd()
 
 token_list = list(pickle.load(open(email_path + "/Data_Pickle/token_dict.p", 'rb')).keys())
 dates = pickle.load(open(email_path + "/Data_Pickle/dates.p", 'rb'))
 tensor = np.load('tensor.npy')
 
-rank = 2
+rank = 12
 cluster_n = 5
 
-
-
-weights, factors = parafac(tl.tensor(tensor), rank = rank, normalize_factors = 1, non_negative = True)
+weights, factors = parafac(tl.tensor(tensor), rank=rank, normalize_factors=True, non_negative=True)
 
 # factors[0]: author
 # factors[1]: months
@@ -52,131 +46,49 @@ for r in range(rank):
 
     tokens.append([token_list[i] for i in index])
 
+######
+# Plot
+######
 
+timeline = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-plt.subplot(211)
-plt.plot(dates, intensity[0], 'r')
-plt.subplot(212)
-plt.plot(dates, intensity[1], 'b')
+fig_per_plot = 7
+
+plt.figure(1)
+
+intensity_index = 0
+subplot_index = 0
+
+colors = cm.rainbow(np.linspace(0, 1, rank + 1))
+
+for i in range(1, rank + 1):
+
+    if subplot_index % fig_per_plot == 0 and i != 1:
+        sub_pos = 1
+        plt.rcParams["figure.figsize"] = (18, 12)
+        plt.savefig(cwd + f'/Gantt/Gantt{i - 1}.png')
+        plt.close()
+        subplot_index = 1
+
+    else:
+        subplot_index += 1
+
+    print(intensity_index, '-', subplot_index)
+    pos = int(str(fig_per_plot) + str(1) + str(subplot_index))
+    plt.subplot(pos)
+    plt.subplots_adjust(hspace=0.4)
+    plt.plot(timeline, intensity[intensity_index], color=colors[i])
+    plt.ylabel('Conversation\nLevel')
+    plt.ylim(0, 1.1)
+    plt.text(-0.45, 0.75, f'Topic {i}', bbox=dict(facecolor='white', edgecolor='white', alpha=0.5))
+    # plt.text(0.5, 0.5, f'California',bbox=dict(facecolor=colors[i], edgecolor = 'white', alpha=0.5))
+    plt.yticks(np.arange(0, 1.1, 0.2))
+
+    plt.grid(True)
+    # plt.title(tokens[intensity_index][0])
+    plt.rcParams["figure.figsize"] = (18, 12)
+
+    intensity_index += 1
+
+plt.savefig(cwd + '/Gantt/Gantt_last.png')
 plt.close()
-
-
-np.linspace(0,11,12)
-
-
-
-
-factors = parafac(tl.tensor(tensor), rank = 1)
-
-
-from scipy.io.matlab import loadmat
-from sktensor import dtensor, cp_als
-
-# Load Matlab data and convert it to dense tensor format
-mat = loadmat('/home/julian/Downloads/brod.mat')
-T = dtensor(mat['X'])
-
-# Decompose tensor using CP-ALS
-P,x, c = cp_als(T, 3, init='random')
-
-from sktensor import dtensor, ktensor
-U = [np.random.rand(i, 3) for i in (2, 2, 2)]
-print(U)
-
-T = dtensor(ktensor(U).toarray())
-
-P, fit, itr = cp_als(T, 3, init = 'random')
-
-np.allclose(T, P.totensor(), atol = 0.001)
-
-
-
-T = np.arange(0,60).reshape((3,4,5))
-
-# Mode 0 unfolding
-T.reshape(3,8)
-
-# Mode 1 unfolding
-T.reshape(4, 6)
-
-# Mode 2 unfolding
-T.reshape(2, 12)
-
-
-####
-# Example Data
-#
-# 3 Authors
-# 5 Terms: Air | Water | Soil | Car | Truck
-# 4 Time Periods
-
-
-####
-
-data = np.array([
-
-        # Author 1
-        [[ 0,  0,  0,  1,  2],
-        [ 0,  0,  0,  3,  4],
-        [0, 0, 0, 9, 14],
-        [0, 0, 0, 10, 10]],
-
-        # Author 2
-       [[10, 14, 15, 0, 0],
-        [8, 8, 12, 0, 0],
-        [5, 0, 5, 0, 0],
-        [0, 0, 4, 0, 0]],
-
-        # Author 3
-       [[5, 5, 5, 0, 0],
-        [6, 6, 6, 6, 6],
-        [33, 23, 15, 10, 12],
-        [0, 0, 0, 12, 15]]
-    ])
-
-
-
-
-T = dtensor(ktensor(data).toarray())
-
-P, fit, itr = cp_als(dtensor(data), 2, init = 'random')
-
-np.allclose(T, P.totensor(), atol = 0.001)
-
-import plotly.figure_factory as ff
-
-df = [dict(Task="Job A", Start='2009-01-01', Finish='2009-02-28', Complete=0),
-      #dict(Task="Job B", Start='2008-12-05', Finish='2009-04-15', Complete=0.5),
-      dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30', Complete=0.86),
-      dict(Task="Job C", Start='2009-06-20', Finish='2009-10-30', Complete=1),
-      ]
-
-colorscale=[
-       [(-0.25, 0), 'blue'],
-       [(0,0.25), 'white'],
-       [(0.25, 1), 'red'],
-            ]
-
-fig = ff.create_gantt(df, colors='Blackbody', index_col='Complete', show_colorbar=True, group_tasks=True)
-fig.show()
-
-
-
-import plotly.plotly as py
-df = [dict(Task="Job-1", Start='2017-01-01', Finish='2017-02-02', Resource='Complete'),
-  dict(Task="Job-1", Start='2017-01-15', Finish='2017-03-15', Resource='Incomplete'),
-  dict(Task="Job-2", Start='2017-01-17', Finish='2017-02-17', Resource='Not Started'),
-  dict(Task="Job-2", Start='2017-01-17', Finish='2017-02-17', Resource='Complete'),
-  dict(Task="Job-3", Start='2017-03-10', Finish='2017-03-20', Resource='Not Started'),
-  dict(Task="Job-3", Start='2017-04-01', Finish='2017-04-20', Resource='Not Started'),
-  dict(Task="Job-3", Start='2017-05-18', Finish='2017-06-18', Resource='Not Started'),
-  dict(Task="Job-4", Start='2017-01-14', Finish='2017-03-14', Resource='Complete')]
-
-colors = {'Not Started': 'rgb(220, 0, 0)',
-      'Incomplete': (1, 0.9, 0.16),
-      'Complete': 'rgb(0, 255, 100)'}
-
-fig = ff.create_gantt(df, colors=colors, index_col='Resource', show_colorbar=True, group_tasks=True)
-
-#py.iplot(fig, filename='gantt-group-tasks-together', world_readable=True)
-
