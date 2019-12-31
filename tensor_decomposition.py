22b93f5f941b34deab82bea29a60a8ba9510a9b0
Shakesbeer333 <julian.metzger.shakesbeer@web.de>
2019-12-31 12:26:04 +0100

    Use sparse arrays instead of dense arrays to save RAM

--- tensor_decomposition.py
+++ tensor_decomposition.py
@@ -1,6 +1,6 @@
 import numpy as np
-import tensorly as tl
-from tensorly.decomposition import parafac
+import tensorly.contrib.sparse as stl
+from tensorly.contrib.sparse.decomposition import parafac as sparse_parafac
 import pickle
 from configparser import ConfigParser
 import os
@@ -13,27 +13,27 @@ email_path = os.path.join(os.getcwd(), dir_)
 cwd = os.getcwd()
 
 token_list = list(pickle.load(open(email_path + "/Data_Pickle/token_dict.p", 'rb')).keys())
-tensor = np.load('tensor.npy')
-tensor = tensor.astype('float32')
+dense_tensor = np.load('tensor.npy')
+dense_tensor = dense_tensor.astype('float32')
+sparse_tensor = stl.tensor(dense_tensor)
 
 rank = parser.getint('Decomposition', 'rank', fallback=25)
 cluster_n = parser.getint('Decomposition', 'cluster_n', fallback=10)
 
-weights, factors = parafac(tl.tensor(tensor), rank=rank, normalize_factors=True, non_negative=True)
+weights, factors = sparse_parafac(sparse_tensor, rank=rank, init='random', normalize_factors=True, non_negative=True)
 
 # factors[0]: author
 # factors[1]: months
 # factors[2]: terms
 
 # Column = Factor
-token_factors = factors[2].T
-time_factors = factors[1].T
+token_factors = factors[2].todense().T
+time_factors = factors[1].todense().T
 
 tokens = []
 intensity = []
 
 for r in range(rank):
-
     loadings = token_factors[r][np.argsort(token_factors[r])[-cluster_n:]]
 
     intensity.append(time_factors[r])
