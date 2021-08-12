ecd77baf5945c5d13fd3428d02d5625ed258b739
Shakesbeer333 <50136885+Shakesbeer333@users.noreply.github.com>
2021-08-12 16:29:59 +0200

    Improved readabilty

--- tensor_decomposition.py
+++ tensor_decomposition.py
@@ -12,8 +12,12 @@ dir_ = parser.get('Parsing', 'dir_', fallback='maildir')
 email_path = os.path.join(os.getcwd(), dir_)
 cwd = os.getcwd()
 
-token_list = list(pickle.load(open(email_path + "/Data_Pickle/token_dict.p", 'rb')).keys())
-employee_list = pickle.load(open(email_path + "/Data_Pickle/employee_list.p", 'rb'))
+token_list = list(pickle.load(open(email_path \
+                                   + "/Data_Pickle/token_dict.p", 'rb')).keys()
+                 )
+employee_list = pickle.load(open(email_path \
+                                 + "/Data_Pickle/employee_list.p", 'rb')
+                           )
 dense_tensor = np.load('tensor.npy')
 dense_tensor = dense_tensor.astype('float32')
 sparse_tensor = stl.tensor(dense_tensor)
@@ -21,7 +25,12 @@ sparse_tensor = stl.tensor(dense_tensor)
 rank = parser.getint('Decomposition', 'rank', fallback=25)
 cluster_n = parser.getint('Decomposition', 'cluster_n', fallback=10)
 
-weights, factors = sparse_parafac(sparse_tensor, rank=rank, init='random', normalize_factors=True, non_negative=True)
+weights, factors = sparse_parafac(sparse_tensor, 
+                                  rank=rank, 
+                                  init='random', 
+                                  normalize_factors=True, 
+                                  non_negative=True
+                                 )
 
 # factors[0]: author
 # factors[1]: months
@@ -51,6 +60,12 @@ for r in range(rank):
 
     intensity.append(time_factors[r])
 
-pickle.dump(tokens, open(email_path + f"/Data_Pickle/conv_tokens{rank}.p", "wb"))
-pickle.dump(intensity, open(email_path + f"/Data_Pickle/conv_intensity{rank}.p", "wb"))
-pickle.dump(author, open(email_path + f"/Data_Pickle/author{rank}.p", "wb"))
+pickle.dump(tokens, 
+            open(email_path + f"/Data_Pickle/conv_tokens{rank}.p", "wb")
+           )
+pickle.dump(intensity,
+            open(email_path + f"/Data_Pickle/conv_intensity{rank}.p", "wb")
+           )
+pickle.dump(author, 
+            open(email_path + f"/Data_Pickle/author{rank}.p", "wb")
+           )
